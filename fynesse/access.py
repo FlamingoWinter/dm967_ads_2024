a30b808c1b5c00f09ca90c2a9639b9a9d1c90fcb
FlamingoWinter <45544452+FlamingoWinter@users.noreply.github.com>
2024-11-05 15:55:40 +0000

    Add download_price_data

--- fynesse/access.py
+++ fynesse/access.py
@@ -7,11 +7,24 @@ import tables
 import mongodb
 import sqlite"""
 
+import requests
+
 # This file accesses the data
 
 """Place commands in this file to access the data electronically. Don't remove any missing values, or deal with outliers. Make sure you have legalities correct, both intellectual property and personal data privacy rights. Beyond the legal side also think about the ethical issues around this data. """
 
 def data():
     """Read the data from the web or local file, returning structured format such as a data frame"""
-    raise NotImplementedError
+    download_price_paid_data(2011, 2020)
 
+def download_price_paid_data(year_from, year_to):
+    base_url = "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com"
+    file_name = "/pp-<year>-part<part>.csv"
+    for year in range(year_from, (year_to+1)):
+        print (f"Downloading data for year: {year}")
+        for part in range(1,3):
+            url = base_url + file_name.replace("<year>", str(year)).replace("<part>", str(part))
+            response = requests.get(url)
+            if response.status_code == 200:
+                with open("." + file_name.replace("<year>", str(year)).replace("<part>", str(part)), "wb") as file:
+                    file.write(response.content)
