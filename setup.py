46c4b7d4d39d7ee2b7f713272eb5708bf206b81d
FlamingoWinter <45544452+FlamingoWinter@users.noreply.github.com>
2024-11-05 23:40:41 +0000

    Package now should install dependencies on load

--- setup.py
+++ setup.py
@@ -26,13 +26,12 @@ REQUIRES_PYTHON = ">=3.6.0"
 VERSION = "0.1.0"
 
 # What packages are required for this module to be executed?
-REQUIRED = [
-    "pandas", "numpy", "jupyter", "matplotlib", 
-]
+with open("requirements.txt") as f:
+    REQUIRED = f.read().splitlines()
 
 # What packages are optional?
 EXTRAS = {
-    "interactive html plots": ["bokeh",],
+        "interactive html plots": ["bokeh", ],
 }
 
 PACKAGE_DATA = {"fynesse": ["defaults.yml"]}
@@ -101,39 +100,39 @@ class UploadCommand(Command):
 
 # Where the magic happens:
 setup(
-    name=NAME,
-    version=about["__version__"],
-    description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-    package_data=PACKAGE_DATA,
-    # If your package is a single module, use this instead of "packages":
-    # py_modules=["mypackage"],
+        name=NAME,
+        version=about["__version__"],
+        description=DESCRIPTION,
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        author=AUTHOR,
+        author_email=EMAIL,
+        python_requires=REQUIRES_PYTHON,
+        url=URL,
+        packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+        package_data=PACKAGE_DATA,
+        # If your package is a single module, use this instead of "packages":
+        # py_modules=["mypackage"],
 
-    # entry_points={
-    #     "console_scripts": ["mycli=mymodule:cli"],
-    # },
-    install_requires=REQUIRED,
-    extras_require=EXTRAS,
-    include_package_data=True,
-    license="MIT",
-    classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy"
-    ],
-    # $ setup.py publish support.
-    cmdclass={
-        "upload": UploadCommand,
-    },
+        # entry_points={
+        #     "console_scripts": ["mycli=mymodule:cli"],
+        # },
+        install_requires=REQUIRED,
+        extras_require=EXTRAS,
+        include_package_data=True,
+        license="MIT",
+        classifiers=[
+                # Trove classifiers
+                # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+                "License :: OSI Approved :: MIT License",
+                "Programming Language :: Python",
+                "Programming Language :: Python :: 3",
+                "Programming Language :: Python :: 3.6",
+                "Programming Language :: Python :: Implementation :: CPython",
+                "Programming Language :: Python :: Implementation :: PyPy"
+        ],
+        # $ setup.py publish support.
+        cmdclass={
+                "upload": UploadCommand,
+        },
 )
