3be4717b0fd6dbcc1673f35a84425f0c1a3bb755
FlamingoWinter <45544452+FlamingoWinter@users.noreply.github.com>
2024-11-25 17:39:56 +0000

    Debugged upload_to_database

--- fynesse/access/upload.py
+++ fynesse/access/upload.py
@@ -20,12 +20,17 @@ def upload_to_database(conn: Connection, table_name: str, df: pd.DataFrame):
     cursor = conn.cursor()
 
     schema = get_schema(df, table_name)
-    create_table_query = schema.replace("CREATE TABLE", "CREATE TABLE IF NOT EXISTS", 1)
+    create_table_query = (schema
+                          .replace("CREATE TABLE", "CREATE TABLE IF NOT EXISTS", 1)
+                          .replace('"', '`'))
+    print(create_table_query)
 
     cursor.execute(create_table_query)
 
     placeholders = ", ".join(["%s"] * len(df.columns))
-    insert_query = f"INSERT INTO {table_name} ({', '.join(df.columns)}) VALUES ({placeholders})"
-    cursor.executemany(insert_query, df.to_records(index=False))
+    columns = ', '.join([f"`{col}`" for col in df.columns])
+    insert_query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
+
+    cursor.executemany(insert_query, [tuple(row) for row in df.to_records(index=False)])
 
     conn.commit()
