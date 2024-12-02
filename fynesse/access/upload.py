53aedb61e1274ba610c054d6847ee2d54e57ec24
FlamingoWinter <45544452+FlamingoWinter@users.noreply.github.com>
2024-12-02 16:17:54 +0000

    propagate join_in_place to fynesse.access

--- fynesse/access/upload.py
+++ fynesse/access/upload.py
@@ -10,8 +10,12 @@ def initialise_database(db_name: str, user: str, password: str, host: str, port:
     return db_setup.initialise_database(db_name, user, password, host, port)
 
 
-def upload_to_database(connection: Connection, table_name: str, df: pd.DataFrame):
-    return db_operations.upload_to_database(connection, table_name, df)
+def upload_to_database(connection: Connection, table_name: str, df: pd.DataFrame, temporary=True):
+    return db_operations.upload_to_database(connection, table_name, df, temporary)
+
+
+def join_in_place(connection, table_name, df_to_join, on):
+    return db_operations.join_in_place(connection, table_name, df_to_join, on)
 
 
 def append_to_database(connection: Connection, table_name: str, df: pd.DataFrame):
