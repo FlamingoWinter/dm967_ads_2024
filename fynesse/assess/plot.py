64e2693be9acf2faf78213be5b764729ed8a6b06
FlamingoWinter <45544452+FlamingoWinter@users.noreply.github.com>
2024-12-03 19:39:30 +0000

    Add correlation_matrix function to assess

--- fynesse/assess/plot.py
+++ fynesse/assess/plot.py
@@ -1,8 +1,15 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def scatter(df, x_column, y_column, alpha=1):
     plt.scatter(df[x_column], df[y_column], alpha=alpha)
     plt.xlabel(x_column)
     plt.ylabel(y_column)
+    plt.show()
+
+
+def correlation_matrix(df):
+    matrix = df.corr()
+    sns.heatmap(matrix, annot=True, cmap="coolwarm", fmt=".2f")
     plt.show()
